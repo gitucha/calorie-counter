52d4b8ebe44c73b95df30127ba8765b837f2f02d
gitucha <briantablet23@gmail.com>
2025-10-02 12:08:52 +0300

    editing paths

--- caloriecounter/urls.py
+++ caloriecounter/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('add_food/', views.add_food, name='add_food'),
     path('remove_food/<int:food_id>/', views.remove_food, name='remove_food'),
     path('calculate_total/', views.calculate_total, name='calculate_total'),
-    path('reset_calories/', views.reset_calories, name='reset_tracker'),   
+    path('reset_calories/', views.reset_calories, name='reset_calories'),   
 ]
